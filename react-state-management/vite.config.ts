f443318c8fbab3788c7364ad5b59deb3296ade2a
Dan Pavitt <dan.pavitt@wirelesslogic.com>
2025-04-10 19:29:40 +0100

    update config for codesubmit

--- react-state-management/vite.config.ts
+++ react-state-management/vite.config.ts
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": resolve(__dirname, "./src"),
     },
   },
   server: {
-    port: 3000,
-    open: true,
+    port: 9000,
+    open: false,
+    host: "0.0.0.0",
+    allowedHosts: true,
   },
 });
